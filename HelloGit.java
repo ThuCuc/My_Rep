37dfa0e0b6d357e75d9bd25b8b24e8d7ccbb03ec
ThuCuc <Thucuc197@gmail.com>
2019-11-05 10:04:10 +0700

    them file HelloGit

---
+++ HelloGit.java
@@ -0,0 +1,8 @@
+public class HelloGit {
+         	public static void main (String [] args) {
+		System.out.println(" Welcome to Version Control System with Git");
+		System.out.println(" Author : Linus Torvald");
+		System.out.println(" Year : 2005");
+		System.out.println(" License : GPL2");
+         	}	
+}
